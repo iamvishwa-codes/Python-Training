37a076cb9e472589300b0017df4ecc2780ba9dc8
VISHWA K <iamvishwa003@gmail.com>
2025-12-17 11:58:37 +0530

    Delete Students directory

--- Students/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import StudentTable
-
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StudentTable
-        fields = ['id', 'name', 'age']
--- Students/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import StudentListAPI, StudentDetailAPI
-
-urlpatterns = [
-    path('student/', StudentListAPI.as_view()),
-    path('student/<int:student_id>/', StudentDetailAPI.as_view()),
-]
